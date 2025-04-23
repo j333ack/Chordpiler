10ed5e2fa193f7f1fbd7e92592d5c417a8de3ecc
Jack Anderson <jackanderson.ja03@gmail.com>
2025-04-23 18:55:27 -0400

    Added Driver and SImpleSongBuilder
    SimpleSongBuilder is the SimpleTableBuilder template from step 3. I changed the name since we're not building symbol tables, but rather songs. The driver has been updated to reflect this change and will not likely need more changes. The SimpleSongBuilder file is where most, if not all, of the work will be done.

---
+++ Driver.java
@@ -0,0 +1,39 @@
+// import ANTLR's runtime libraries
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+//YOU ARE NOT REQUIRED TO MODIFY THIS CLASS
+
+public class Driver {
+	
+	public static void main(String[] args) throws Exception {
+
+		// create a CharStream that reads from standard input
+		ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+		// create a lexer that feeds off of input CharStream
+		guitartabLexer lexer = new guitartabLexer(input);
+
+		// create a buffer of tokens pulled from the lexer
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		
+		// create a parser that feeds off the tokens buffer
+		guitartabParser parser = new guitartabParser(tokens);
+
+		ParseTree tree = parser.song(); // begin parsing at prog rule
+
+		// Create a generic parse tree walker that can trigger callbacks
+        ParseTreeWalker walker = new ParseTreeWalker();
+
+		//create a symbol table object stb
+		SimpleSongBuilder stb = new SimpleSongBuilder();
+
+        // Walk the tree created during the parse, trigger callbacks
+        walker.walk(stb, tree);
+		
+		// print the symbol table entries
+		stb.prettyPrint();
+
+		//YOU ARE NOT REQUIRED TO ADD ANY CODE HERE
+	}
+}
---
+++ SimpleSongBuilder.java
@@ -0,0 +1,9 @@
+public class SimpleSongBuilder extends guitartabBaseListener 
+{
+
+	public void prettyPrint()
+	{
+           //print all symbol tables in the order they were created
+	       System.out.println("prettyPrint(): Not Implemented");
+	}
+}
