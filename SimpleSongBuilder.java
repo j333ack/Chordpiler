05cf1b67f2ae56d2f3c1bd93e68f11ea2e38144b
Jack Anderson <jackanderson.ja03@gmail.com>
2025-04-23 20:55:20 -0400

    Patch 0.0.2
    Added some classes

--- SimpleSongBuilder.java
+++ SimpleSongBuilder.java
@@ -1,9 +1,72 @@
+import java.util.*;	
+	
+	// Song class
+	class Song
+	{
+		private LinkedList<Chord> chords = new LinkedList<>;
+		private int bpm;
+		
+		public Song()
+		{
+			bpm = 120;
+		}
+		
+		public Song(int bpm)
+		{
+			this.bpm = bpm;
+		}
+		
+		public int getBpm()
+		{
+			return bpm;
+		}
+		
+		public void setBpm(int bpm)
+		{
+			this.bpm = bpm;
+		}
+	}
+	
+	// Chord class
+	class Chord
+	{
+		private int notes[6];
+		private int strums;
+		
+		public Chord()
+		{
+			strums = 1;
+		}
+		
+		public Chord(int strums)
+		{
+			this.strums = strums;
+		}
+		
+		public int getStrums()
+		{
+			return strums;
+		}
+		
+		public void setStrums(int strums)
+		{
+			this.strums = strums;
+		}
+	}
+	
+	// Channel class
+	class Channel
+	{
+		private LinkedList<int> notes = new LinkedList<>;
+		
+		public Channel(){}
+	}
+
 public class SimpleSongBuilder extends guitartabBaseListener 
 {
-
 	public void prettyPrint()
 	{
-           //print all symbol tables in the order they were created
+           // Print all symbol tables in the order they were created
 	       System.out.println("prettyPrint(): Not Implemented");
 	}
 }
