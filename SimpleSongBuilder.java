4ff7e046ea929a42a28170f438e41ef33abf9890
Jack Anderson <jackanderson.ja03@gmail.com>
2025-04-27 02:45:40 -0400

    Added user manual, example outputs, and cleaned code for readability
    Made SimpleSongBuilder.java more readable with proper formatting, did not add comments tho. Demo video coming soon

--- SimpleSongBuilder.java
+++ SimpleSongBuilder.java
@@ -1,12 +1,7 @@
 import java.util.*;	
 	
-
-
-
 public class SimpleSongBuilder extends guitartabBaseListener 
 {
-
-	//LinkedList<Integer>[] numbers = new LinkedList<Integer>[6] ;
 	ArrayList<LinkedList<Integer>> numbers = new ArrayList<LinkedList<Integer>>();
 	LinkedList<Integer> strumNums = new LinkedList<>();
 	int stringCount = 0;
@@ -17,41 +12,17 @@ public class SimpleSongBuilder extends guitartabBaseListener
 	int strums;
 	int bpm = 120;
 
-public SimpleSongBuilder() {
-        for (int i = 0; i < 6; i++) {
-            numbers.add(new LinkedList<>());
-        }
+public SimpleSongBuilder() 
+{
+	for (int i = 0; i < 6; i++)
+	{
+		numbers.add(new LinkedList<>());
     }
+}
 
 public void prettyPrint()
-    {
-        // Print the contents of a tidal file to stdout. The Micro.sh script can route it to a file
-        //System.out.println("prettyPrint(): Not Implemented");
-		
-        /* System.out.print("hush\n\n");
-        System.out.print("setcps(" + bpm + "/60/4)\n\n");
-        for (int i = 0; i < 6; i++)
-        {
-            System.out.print("d" + (i+1) + " $ slow " + numbers.get(i).size() + " $ note \"");
-            for (Integer noteValue : numbers.get(i))
-            {
-                System.out.print("[");
-				int chordNum = 0;
-                for (int j = 0; j < strumNums.get(chordNum); j++)
-                {
-					if (noteValue != null)
-						System.out.print(noteValue + " ");
-					else
-						System.out.print("~ ");
-					chordNum++;
-                }
-                System.out.print("] ");
-			
-            }
-            System.out.print("\" # sound \"superpiano\"\n\n");
-        } */
-		
-		System.out.print("hush\n\n");
+{
+        System.out.print("\nhush\n\n");
 		System.out.print("setcps(" + bpm + "/60/4)\n\n");
 		for (int i = 0; i < 6; i++)
         {
@@ -73,80 +44,65 @@ public void prettyPrint()
     }
 
 	@Override
-	public void enterString(guitartabParser.StringContext ctx){
-
-
-	isFound = false;
-	fretCount = 0;
-
-
+	public void enterString(guitartabParser.StringContext ctx)
+	{
+		isFound = false;
+		fretCount = 0;
 	}
 
 	@Override
-	public void exitString(guitartabParser.StringContext ctx){
-
-	if(isFound == false){
-		numbers.get(stringCount).add(currentNote);
-	}
-
-	stringCount++;
+	public void exitString(guitartabParser.StringContext ctx)
+	{
+		if(isFound == false)
+			numbers.get(stringCount).add(currentNote);
+		stringCount++;
 	}
 
 	@Override
-	public void enterBpm(guitartabParser.BpmContext ctx){
-
+	public void enterBpm(guitartabParser.BpmContext ctx)
+	{
 		bpm = Integer.parseInt(ctx.getText());
-
 	}
 
 	@Override
-	public void enterNote(guitartabParser.NoteContext ctx){
+	public void enterNote(guitartabParser.NoteContext ctx)
+	{
 		currentNote = noteMap(ctx.getText());
 	}
 
-
 	@Override
-	public void enterFinger(guitartabParser.FingerContext ctx){
-	String finger = ctx.getText();
-	if(finger.equals("x")){
-		numbers.get(stringCount).add(currentNote + fretCount);
+	public void enterFinger(guitartabParser.FingerContext ctx)
+	{
+		String finger = ctx.getText();
+		if(finger.equals("x"))
+			numbers.get(stringCount).add(currentNote + fretCount);
+		else
+			numbers.get(stringCount).add(null);
+		isFound = true;
 	}
-	else{
-		numbers.get(stringCount).add(null);
-	}
-
-	isFound = true;
-	}
-
 
 	@Override
 	public void enterTab(guitartabParser.TabContext ctx)
-{
-
+	{
 		chordCount++;
 		stringCount = 0;
 		String num = ctx.num_strums().getText();
-		if(!num.equals("")){
+		if(!num.equals(""))
 			strumNums.add(Integer.parseInt(num));
-		}
-		else{
+		else
 			strumNums.add(1);
-		}
 	}
 
 	@Override
-	public void enterPosition(guitartabParser.PositionContext ctx){
-
-	fretCount++;
-
+	public void enterPosition(guitartabParser.PositionContext ctx)
+	{
+		fretCount++;
 	}
 
-	private Integer noteMap(String note){
-
-
-
-		switch(note){
-
+	private Integer noteMap(String note)
+	{
+		switch(note)
+		{
 			case "e":
 				return 4;
 			case "B":
